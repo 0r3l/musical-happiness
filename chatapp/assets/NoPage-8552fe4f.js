030daf2d708c6b7ab36caea6be2f40a75b6db0e1
Orel Eliyahu <42113188+0r3l@users.noreply.github.com>
2024-09-22 21:46:04 +0300

    Add files via upload

---
+++ chatapp/assets/NoPage-8552fe4f.js
@@ -0,0 +1,2 @@
+import{bn as n}from"./vendor-0ef69949.js";function o(){return n.jsx("h1",{children:"404"})}o.displayName="NoPage";export{o as Component};
+//# sourceMappingURL=NoPage-8552fe4f.js.map
